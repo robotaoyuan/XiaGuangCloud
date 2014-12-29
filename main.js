66ce8401813660fedf21e00740784933176ab3de
Yuan Tao <robotaoyuan@gmail.com>
2014-12-29 12:22:12 +0800

    first

---
+++ main.js
@@ -0,0 +1,10 @@
+// Use AV.Cloud.define to define as many cloud functions as you want.
+// For example:
+AV.Cloud.define("hello", function(request, response) {
+  response.success("Hello world!");
+});
+
+
+AV.Cloud.define("youmi", function(request, response) {
+  response.success("Hello world");
+});
