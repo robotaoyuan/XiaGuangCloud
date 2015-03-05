23998712f9bf9a989421290456a8f0752a56cd85
KeZhuoPeng <YunTop@YunTopdeMacBook-Pro.local>
2015-03-05 13:45:35 +0800

    add dianping

--- cloud/app.js
+++ cloud/app.js
@@ -1,5 +1,7 @@
 // 在 Cloud code 里初始化 Express 框架
 var express = require('express');
+var crypto = require('crypto');
+var querystring = require('querystring');
 var app = express();
 
 // App 全局配置
@@ -42,6 +44,127 @@ app.get('/youmi', function(req, res){
 	res.json({});
 });
 
+app.get('/dianping_has_groupBuying',function(req,res){
+    var json = {};
+    var shopId = req.param("shopId");
+    if (shopId == undefined) {
+        res.json({});
+    }
+    avCloudHttp(shopId,function(has){
+        if(has != null){
+            json = {
+                status: 'OK',
+                has : has
+            };
+        }else{
+            json = {
+                status: 'Error',
+                message: 'return error'
+            };
+        }
+        res.json(json);
+    });
+});
 
+app.post('/update_xiaguang_cloud',function(req,res){
+    var shopId = req.body.shopId;
+    var objectId = req.body.objectId;
+    if (shopId == undefined || objectId == undefined){
+        res.json({
+            'status':'Error',
+            'message':'无效参数shopId或objectId'
+        });
+    }
+    shopId = parseInt(shopId);
+    AV.Cloud.httpRequest({
+        method:'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        url:'http://xiaGuang.avosapps.com//dianping_has_groupBuying?shopId=' + shopId,
+        success: function(response){
+            var json = JSON.parse(response.text);
+            if (json['status'] == "OK"){
+                var has_deal = json['has'];
+                AV.Cloud.httpRequest({
+                    method: 'PUT',
+                    url: 'https://leancloud.cn/1.1/classes/Merchant/' + objectId,
+                    headers:{
+                        'Content-Type': 'application/json',
+                        'X-AVOSCloud-Application-Id': 'p8eq0otfz420q56dsn8s1yp8dp82vopaikc05q5h349nd87w',
+                        'X-AVOSCloud-Application-Key': 'kzx1ajhbxkno0v564rcremcz18ub0xh2upbjabbg5lruwkqg'
+                    },
+                    body: {
+                        'has_deal':has_deal
+                    },
+                    success: function(response){
+                        res.json({
+                            'status':'OK',
+                            'message':'Update success'
+                        });
+                    },
+                    error: function(response){
+                        res.json({
+                            'status':'Error',
+                            'message':'Update Error'
+                        });
+                    }
+                });
+
+            }
+        },
+        error: function(response){
+            res.json({
+                'status': 'Error',
+                'message': '请求错误'
+            });
+        }
+    });
+});
+
+function avCloudHttp(shopId,callBack){
+    //参数列表
+    var param = {};
+    param["business_id"] = shopId;
+    param["sign"] = appSign(param,_dzdpAppKey,_dzdpSecret);
+    param["appkey"] = _dzdpAppKey;
+
+    AV.Cloud.httpRequest({
+        method:'GET',
+        url:'http://api.dianping.com/v1/business/get_single_business?' + querystring.stringify(param),
+        success:function(httpResults){
+            var result = JSON.parse(httpResults.text);
+            var _has;
+            if (result['status'] == 'OK'){
+                var businesses = result['businesses'][0];
+                callBack(businesses['has_deal']);
+            }
+        },
+        error: function(httpResults){
+            callBack(null);
+        }
+    });
+}
+
+function appSign(param,appkey,secret){
+    var array = new Array();
+    for (var key in param) {
+        array.push(key);
+    }
+    array.sort();
+
+    var paramArray = new Array();
+    paramArray.push(appkey);
+    for (var index in array){
+        var key = array[index];
+        var paramKey = param[key];
+        paramArray.push(key + paramKey);
+    }
+    paramArray.push(secret);
+
+    var shaSource = paramArray.join("");
+    var sign =  crypto.createHash('sha1').update(shaSource , 'utf8').digest('hex').toUpperCase();
+    return sign;
+}
 // 最后，必须有这行代码来使 express 响应 HTTP 请求
 app.listen();
--- cloud/main.js
+++ cloud/main.js
@@ -4,3 +4,46 @@ require("cloud/app.js");
 AV.Cloud.define("hello", function(request, response) {
   response.success("Hello world!");
 });
+
+AV.Cloud.define("RefreshMeachantDealData",function(req,res){
+    var param = {
+        shopId:{'$exists':true,'$nin':[""," "]}
+    };
+    AV.Cloud.httpRequest({
+        method:'GET',
+        url:'https://leancloud.cn/1.1/classes/Merchant?where=' + JSON.stringify(param),
+        headers:{
+            'Content-Type': 'application/json',
+            'X-AVOSCloud-Application-Id': 'p8eq0otfz420q56dsn8s1yp8dp82vopaikc05q5h349nd87w',
+            'X-AVOSCloud-Application-Key': 'kzx1ajhbxkno0v564rcremcz18ub0xh2upbjabbg5lruwkqg'
+        },
+        success:function(response){
+            var result = JSON.parse(response.text);
+            for (var index in result["results"]){
+                var object = result["results"][index];
+                AV.Cloud.httpRequest({
+                    method:'POST',
+                    url:"http://xiaGuang.avosapps.com/updateCloud",
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: {
+                        objectId:object['objectId'],
+                        shopId:object['shopId']
+                    },
+                    success:function(response){
+                        console.log(response.text);
+                    },
+                    error:function(response){
+                        console.log('更新错误');
+                    }
+
+                });
+            }
+
+        },
+        error:function(response){
+            console.log('查找Merchant错误');
+        }
+    });
+});
